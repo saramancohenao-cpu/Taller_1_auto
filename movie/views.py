d1a7d768f4a41f1648dc64aa5dd3ae5563eea665
sara <saramancohenao@gmail.com>
2025-09-05 11:13:41 -0500

    cambios en la funcion about

--- movie/views.py
+++ movie/views.py
@@ -9,4 +9,5 @@ def home (request):
 
 
 def about(request):
-    return HttpResponse('<h1>Welcome to about page</h1>')
+    #return HttpResponse('<h1>Welcome to about page</h1>')
+    return render(request, 'about.html')
